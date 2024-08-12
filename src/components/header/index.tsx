dad7dfbd3f99e68194c0f808136e22529ad3edc6
Kayo <Kayohenriquesilva19@gmail.com>
2024-08-11 22:51:51 -0300

    style: table ui

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,14 @@
+import { Link } from 'react-router-dom';
 import styles from './header.module.css';
 
 export function Header() {
   return (
     <header className={styles.container}>
-      <h1 className={styles.title}>
-        Crypto <span className={styles.emphasis}>Seek</span>
-      </h1>
+      <Link to={'/'}>
+        <h1 className={styles.title}>
+          Crypto <span className={styles.emphasis}>Seek</span>
+        </h1>
+      </Link>
     </header>
   );
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,32 @@
-import { Search, X } from 'lucide-react';
+import { Search, TrendingUp, X } from 'lucide-react';
+import { useEffect, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
 import styles from './home.module.css';
 
 export function Home() {
+  const [isActive, setIsActive] = useState(false);
+  const formRef = useRef<HTMLFormElement | null>(null);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (formRef.current && !formRef.current.contains(event.target as Node)) {
+        setIsActive(false);
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [formRef]);
+
   return (
     <main className={styles.mainContainer}>
-      <form className={styles.formContainer}>
+      <form
+        className={`${styles.formContainer} ${isActive ? styles.active : ''}`}
+        onClick={() => setIsActive(true)}
+        ref={formRef}
+      >
         <button type="submit" className={styles.buttonForm}>
           <Search />
         </button>
@@ -17,6 +39,46 @@ export function Home() {
           <X />
         </button>
       </form>
+
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">Moeda</th>
+            <th scope="col">Valor mercado</th>
+            <th scope="col">Preço</th>
+            <th scope="col">Volume</th>
+            <th scope="col">Mudança 24h</th>
+          </tr>
+        </thead>
+
+        <tbody id="tbody">
+          <tr className={styles.tr}>
+            <td className={styles.tdLabel} data-Label="Moeda">
+              <div className={styles.name}>
+                <Link to={'/details/bitcoin'}>
+                  <span>Bitcoin | BTC</span>
+                </Link>
+              </div>
+            </td>
+
+            <td className={styles.tdLabel} data-Label="Valor mercado">
+              1T
+            </td>
+            <td className={styles.tdLabel} data-Label="Preço">
+              8.000
+            </td>
+            <td className={styles.tdLabel} data-Label="Volume">
+              2B
+            </td>
+            <td className={styles.tdProfit} data-Label="Mudança 24h">
+              <div className={styles.change}>
+                <span>1.20</span>
+                <TrendingUp />
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </main>
   );
 }
